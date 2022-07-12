37454bf077052c4d0414250ccb1e35c4c869ecd4
shohaha <96545188+shohaha@users.noreply.github.com>
2022-07-12 20:41:41 +0100

    update index html, css, scss and js

--- static/js/style.js
+++ static/js/style.js
@@ -1,8 +1,25 @@
 // Toggle Animation by Class
-$(window).scroll(function(){
-    if($(document).scrollTop() > 100){
-      $('nav').addClass('animate');
-    }else{
-      $('nav').removeClass('animate');
-    }
-  });
+
+;(function($) {
+  $(function() {
+    $('nav ul li > a:not(:only-child)').click(function(e) {
+      $(this)
+        .siblings('.nav-dropdown')
+        .slideToggle()
+      $('.nav-dropdown')
+        .not($(this).siblings())
+        .hide()
+      e.stopPropagation()
+    })
+    $('html').click(function() {
+      $('.nav-dropdown').hide()
+    })
+    // Toggle open and close nav styles on click
+    $('#nav-toggle').click(function() {
+      $('nav ul').slideToggle();
+    });
+    $('#nav-toggle').on('click', function() {
+      this.classList.toggle('active')
+    })
+  })
+})(jQuery)
